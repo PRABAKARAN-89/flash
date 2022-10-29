30b80d9ebe15acd3c57a0e2b2d97241a2db42a51
admin <admin@DESKTOP-E4086EQ>
2022-10-29 07:04:40 +0530

    First Commit Uploading all files

---
+++ GitDemo/src/in/demo/Addition.java
@@ -0,0 +1,15 @@
+package in.demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+			
+		final int numberFirst = 10;
+		final int numberSecond = 10;
+		final int result = numberFirst + numberSecond;
+		
+		System.out.println("Result is: " +result);
+
+	}
+
+}
